e4e5a18123dac14bf1411f72942493aa599b49f1
Ricardo-gib <2101080231@undc.edu.pe>
2025-11-12 01:02:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// sw.js (en /LexDigital/)
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {}); // necesario para que Chrome lo considere
+self.addEventListener('fetch', () => {}); 
