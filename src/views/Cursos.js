37adf89bdb22d134999a8ceb11b0ea47aab6f5da
Ricardo-gib <2101080231@undc.edu.pe>
2025-11-26 00:20:22 -0500

    Delete src directory

--- src/views/Cursos.js
+++
@@ -1,9 +0,0 @@
-import TabBar from '../components/TabBar.js';
-export default function Cursos(){
-  const html = `
-  <div class="container">
-    <div class="card"><h1>Cursos</h1><p>Próximamente.</p></div>
-    ${TabBar('cursos')}
-  </div>`;
-  return { html };
-}
